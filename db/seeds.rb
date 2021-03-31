9fd0fdced2baf301bb50b89cb0ef733b2f4ae2de
Shirley Alvarado <shirley@codecrc.com>
2021-03-30 20:40:15 -0600

    done first part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+karl = User.create!(name: 'Karl', picture: 'https://karlheitmann.github.io/assets/img/yo_terraza.jpg', email: 'karl@example.com', password: 'asdasd')
+
+50.times do |i|
+  Tweet.create!(user_id: karl.id, content: Faker::Lorem.paragraph(sentence_count: 2))
+end
+
+10.times do |j|
+  user = User.create!(name: "user #{j}", picture: 'https://picsum.photos/200/300', email: "user#{j}@example.com", password: 'asdasd')
+
+  50.times do |i|
+    Tweet.create!(user_id: user.id, content: Faker::Lorem.paragraph(sentence_count: 2))
+  end
+end
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
